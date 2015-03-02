5c9b6352dbccb9fcdf09aeec10cd31e03c6847bd
Tiffany Chiou <tiffanichiou@gmail.com>
2015-03-02 14:08:13 -0800

    Began implementing AJAX, saving this version before debugging more

--- accounts.php
+++ accounts.php
@@ -1,32 +1,40 @@
 <?php
-
+/*
 require("../cred.php");
 
 $connect = new mysqli($host, $user, $pd, $db);
 if ($connect->connect_errno) {
   echo "Connection failed: $connect->connect_errno \n";
 } // this connects to the database
+*/
+$userData = file_get_contents("php://input");
 
-if (isset($_POST['first'])) {
-  $fName = $_POST['first'];
-  $lName = $_POST['last'];
-  $user = $_POST['username'];
-  $word = $_POST['pword'];
+var_dump(json_decode($userData));
+
+
+/*
+$fName = $user->data['first'];
+$lName = $user->data['last'];
+$username = $user->data['user'];
+$word = $user->data['word'];
 
 if (!($addUser = $connect->prepare("INSERT INTO credentials(first, last, user, word) VALUES (?, ?, ?, ?)"))) {
   echo "Prep error";
 }
 
-if (!($addUser->bind_param("ssss", $fName, $lName, $user, $word))) {
+if (!($addUser->bind_param("ssss", $fName, $lName, $username, $word))) {
   echo "Param error";
 }
 
 if (!($addUser->execute())) {
   echo "Execution error";
-} // add the user if they sign up
-
-$addUser->close();
+} else {
 
 
 }
+
+$addUser->close();
+
+*/
+
 
