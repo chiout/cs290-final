0c79767bfae5c70a041c606445a0419c99c58da0
Tiffany Chiou <tiffanichiou@gmail.com>
2015-03-02 18:52:43 -0800

    Finished debugging Ajax

--- accounts.php
+++ accounts.php
@@ -6,20 +6,14 @@ $connect = new mysqli($host, $user, $pd, $db);
 if ($connect->connect_errno) {
   echo "Connection failed: $connect->connect_errno \n";
 } // this connects to the database
-/*
-$userData = file_get_contents("php://input");
-$user = json_decode($userData);
-
-var_dump($user);
-*/
 
 $fName = $_POST['firstName'];
 $lName = $_POST['lastName'];
 $username = $_POST['username'];
 $word = $_POST['password'];
 
-echo $username;
-/*
+echo $_POST['username'];
+
 if (!($addUser = $connect->prepare("INSERT INTO credentials(first, last, user, word) VALUES (?, ?, ?, ?)"))) {
   echo "Prep error";
 }
@@ -29,11 +23,11 @@ if (!($addUser->bind_param("ssss", $fName, $lName, $username, $word))) {
 }
 
 if (!($addUser->execute())) {
-  echo "Execution error".$fName;
+  echo "Execution error";
 } else {
   echo "Done!";
 }
 
-$addUser->close();*/
+$addUser->close();
 
 
